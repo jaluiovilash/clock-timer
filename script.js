de65cf306e0fefb24ed156f97c474942bf3ea25f
jaluiovilash <ovilashjalui@gmail.com>
2024-05-22 11:34:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// 2ways of writing clock
+const clock = document.getElementById('clock');
+// const clock = document.querySelector('#clock');
+
+
+setInterval(function () {
+    let date = new Date();
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+
+}, 1000)
